c164e80fb65ef488eef9096c0f4721926382f374
beefybroccoli <tedkimemail@yahoo.com>
2021-07-22 19:22:55 -0700

    (no comment)

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ class Airplane {
 */
 
 /**
+ *  - Write a Person class whose constructor initializes `name` and `age` from arguments.
+    - All instances of Person should also initialize with an empty `stomach` array.
  * @param name
  * @param age
  */
@@ -52,14 +54,31 @@ class Person {
     this.stomach = [];
     this.stomach_size = 10;
   }
+  /**
+   * - Give instances of Person the ability to `.eat("someFood")`:
+        + When eating an edible, it should be pushed into the `stomach`.
+        + The `eat` method should have no effect if there are 10 items in the `stomach`.
+   * @param {*} food 
+   */
   eat(food) {
     if (this.stomach.length < this.stomach_size) {
       this.stomach.push(food);
     }
   }
+
+  /**
+   *  - Give instances of Person the ability to `.poop()`:
+        + When an instance poops, its `stomach` should empty.
+   */
   poop(){
     this.stomach=[];
   }
+
+  /**
+   * - Give instances of Person a method `.toString()`:
+        + It should return a string with `name` and `age`. Example: "Mary, 50"
+   * @returns  `${this.name}, ${this.age}`;
+   */
   toString(){
     return `${this.name}, ${this.age}`;
   }
