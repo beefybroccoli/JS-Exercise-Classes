8ea6ed159ced99e7d8d8deb7e383fdea24319308
beefybroccoli <tedkimemail@yahoo.com>
2021-07-22 19:44:33 -0700

    (no comment)

--- index.js
+++ index.js
@@ -100,6 +100,7 @@ class Person {
 */
 
 class Car {
+
   /**
    * - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
     - All instances built with Car:
@@ -113,7 +114,7 @@ class Car {
     this.odometer = 0;
     this.milesPerGallon = milesPerGallon;
     this.model = model;
-  }
+  }//end constructor
   
   /**
    * - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
@@ -121,7 +122,7 @@ class Car {
    */
   fill(gallons){
     this.tank += gallons;
-  }
+  }//end fill method
 
   /**
    * - Give cars ability to `.drive(distance)`. The distance driven:
@@ -129,30 +130,32 @@ class Car {
         + Should cause the the `tank` to go down taking `milesPerGallon` into account.
     - A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+    - drive method when NOT enough fuel increases miles by drivable miles
+    - drive method when NOT enough fuel empties the tank (1 ms)
    * @param {*} distance 
    * @returns "I ran out of fuel at {odometer} miles!"
    */
   drive(distance){
+    
     //run out of gas case
     if(distance > this.milesPerGallon * this.tank) {
 
-      // [9] drive method when NOT enough fuel increases miles by drivable miles
-      this.odometer = this.tank * this.milesPerGallon;
+      // odometer = odometer + drivable miles
+      this.odometer += this.tank * this.milesPerGallon;
+      // empty tank
       this.tank = 0;
 
-      // ✕ [10] drive method when NOT enough fuel empties the tank (1 ms)
-
-      return `I ran out of fuel at ${this.odometer + this.tank * this.milesPerGallon} miles!`;
+      returns `I ran out of fuel at ${this.odometer + this.tank * this.milesPerGallon} miles!`;
     } 
     //sufficient gas case
     else{
       this.odometer += distance;
       this.tank -= distance/this.milesPerGallon;
-      
     }
-  }
+
+  }//drive method
   
-}
+}//end car class
 
 /*
   TASK 3
@@ -243,6 +246,7 @@ class Instructor extends Lambdasian{
   }
 
 }
+
 /*
   TASK 5
     - Write a Student class extending Lambdasian.
